84fbe075ad2af87047725de2d7037b36cc21fe62
bounceb <itsbounceb@gmail.com>
2025-10-13 16:18:09 +1000

    website

--- js/about.js
+++ js/about.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
 const placeholder = [
 	"jeff bezos has 121 billion dollars",
-	"i hope you never find love",
+	"you should be hunted for sport",
 	"nothing can stop me now",
 	"you don't get to see the kids this weekend",
-	"about that beer i owed ya",
 	"go FUCK yourself andrew",
-	"there is something seriously wrong with you",
 	"your music's shite, it keeps me up all night",
+	"i hate everything you stand for",
+	"hello i'm GOD and i FORGIVE YOU",
 ];
-const textarea = document.getElementById('telecommunication');
+const textarea = document.getElementById('messagebox');
 const rng = Math.floor(Math.random() * placeholder.length);
 const newPlaceholder = placeholder[rng];
 textarea.setAttribute('placeholder', newPlaceholder);
 });
 
 function sendMessage() {
-	document.getElementById("telecommunication").value = "";
+	document.getElementById("messagebox").value = "";
 	setTimeout(() => {
 		alert('pigeon sent out!');
 	}, 0);
